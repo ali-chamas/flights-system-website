f65ce5006ce97a6133459303367fa55d70c51d60
watwatos <alichamas.22@hotmail.com>
2024-03-17 18:01:04 +0200

    rate-airline-api

---
+++ server/airlines/rateAirlines.php
@@ -0,0 +1,48 @@
+<?PHP
+ 
+ include("../db/connection.php");
+
+ 
+ $request_method = $_SERVER["REQUEST_METHOD"];
+
+ if($request_method=="POST"){
+
+  
+
+    $airlineID = intval($_POST["airlineID"]);
+    $userID=intval($_POST["userID"]);
+    $rate= $_POST["rating"];
+    $review= $_POST["review"];
+    
+    
+    $response = rateAirlines($userID,$airlineID,$rate, $review);
+    echo json_encode($response);
+
+
+
+ }
+ function rateAirlines($userID,$airlineID,$rate, $review){
+  global $mysqli; 
+  $query = $mysqli->prepare("insert into airlinesreviews(rating,review,userID,airlineID) values(?,?,?,?)");
+  $query->bind_param("ssii",$rate,$review, $userID,$airlineID);
+  if($query->execute()){
+      $updateAverage=$mysqli->prepare("
+      update airlines as a
+      Set rating=(
+        select AVG(r.rating)
+        from airlinesreviews as r
+        where a.id=r.airlineID
+      )
+      where a.id=?
+      ");
+      $updateAverage->bind_param("i",$userID);
+      if($updateAverage->execute())
+      $response["status"]= "success";
+
+      else 
+      $response["status"]="failed";
+  }else{
+      $response["status"]= "failed";
+  }
+  return $response;
+}
