c912f34a58f4eb52c9b056bc6832bc4e0f5af3e2
unknown <hanadi.hosri@gmail.com>
2024-03-18 00:53:11 +0200

    implementing add.php

--- server/tickets/add.php
+++ server/tickets/add.php
@@ -3,3 +3,19 @@
 include("../db/connection.php");
 
 $request_method = $_SERVER["REQUEST_METHOD"];
+
+$totalSeats = $_POST['totalSeats'];
+$date = $_POST['date'];
+$price = $_POST['price'];
+
+$query = $mysqli->prepare('insert into tickets(totalSeats, date, price) values(?,?,?);');
+$query->bind_param('isd', $totalSeats, $date, $price);
+if ($query->execute()){
+    $response['status'] = "success";
+    $response['message'] = "ticket was added successfully";
+} else {
+    $response['status'] = "failed";
+    $response['message'] = "ticket wasn't added";
+}
+
+echo json_encode($response);
