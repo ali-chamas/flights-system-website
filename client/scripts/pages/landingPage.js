d62d0f3afe36498078b151b1c9e5c3d5029665c9
watwatos <alichamas.22@hotmail.com>
2024-03-18 20:10:36 +0200

    fixed-apiURL

---
+++ client/scripts/pages/landingPage.js
@@ -0,0 +1,24 @@
+const destinationInput = document.getElementById("destination-input");
+const searchBtn = document.getElementById("search-btn");
+
+let searchValue = "";
+
+const searchFlight = async (destination) => {
+  try {
+    const res = await fetch(
+      `${apiURL}/flights/searchFlight.php?destination=${destination}`
+    );
+    const data = await res.json();
+    window.location.assign(
+      `/client/pages/flights/singleFlightPage.html?id=${data.id}`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+destinationInput.addEventListener("change", (e) => {
+  searchValue = e.target.value;
+});
+
+searchBtn.addEventListener("click", () => searchFlight(searchValue));
---
+++ server/flights/searchFlight.php
@@ -0,0 +1,28 @@
+<?php
+
+include("../db/connection.php");
+
+$destination=$_GET["destination"];
+
+
+$query = $mysqli->prepare('
+SELECT id from flights where destination = ?
+
+');
+$query->bind_param('s', $destination);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+
+if ($num_rows == 0) {
+    $response['status'] = "no tickets found";
+} else {
+   
+    $query->bind_result($id);
+    $query->fetch();
+
+    $response['status'] = "success";
+    $response['id'] = $id;
+}
+
+echo json_encode($response);
