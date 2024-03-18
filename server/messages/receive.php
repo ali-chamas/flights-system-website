84e8d964c796a2d01ab5d5418750e540d54b6751
watwatos <alichamas.22@hotmail.com>
2024-03-18 07:46:34 +0200

    fixed-receive-api

--- server/messages/receive.php
+++ server/messages/receive.php
@@ -5,24 +5,62 @@ include("../db/connection.php");
 $request_method = $_SERVER["REQUEST_METHOD"];
 
 
-$query = $mysqli->prepare('select message from messages');
-$query->execute();
-$query->store_result();
-$num_rows = $query->num_rows();
+if(!empty($_GET['id'])){
 
-if ($num_rows == 0) {
-    $response["status"] = "no messages found";
-} else {
-    $messagesInfo = [];
-    $query->bind_result($message);
-    while($query->fetch()) {
-        $messageInfo = ['message'=> $message];
-        $messagesInfo[] =$messageInfo;
+    $id=$_GET['id'];
+    $query = $mysqli->prepare('select message,sentAT,userID,receiver from messages Where userID=? or receiver = ?');
+    $query->bind_param('ii',$id,$id);
+    $query->execute();
+    $query->store_result();
+    $num_rows = $query->num_rows();
+
+    if ($num_rows == 0) {
+        $response["status"] = "no messages found";
+    } else {
+        $messages = [];
+        $query->bind_result($message,$sentAt,$userID,$receiver);
+        while($query->fetch()) {
+            $message=[
+                'message'=>$message,
+                'sentAt'=>$sentAt,
+                'sender'=>$userID,
+                'receiver'=>$receiver
+            ];
+            $messages[] =$message;
+        }
+
+        $response["status"] = "success";
+        $response["messages"] = $messages;
+
     }
 
-    $response["status"] = "success";
-    $response["messagesInfo"] = $messagesInfo;
+    echo json_encode($response);  
 
+}else{
+    $query = $mysqli->prepare('select message,sentAT,userID,receiver from messages');
+    $query->execute();
+    $query->store_result();
+    $num_rows = $query->num_rows();
+
+    if ($num_rows == 0) {
+        $response["status"] = "no messages found";
+    } else {
+        $messages = [];
+        $query->bind_result($message,$sentAt,$userID,$receiver);
+        while($query->fetch()) {
+            $message=[
+                'message'=>$message,
+                'sentAt'=>$sentAt,
+                'sender'=>$userID,
+                'receiver'=>$receiver
+            ];
+            $messages[] =$message;
+        }
+
+        $response["status"] = "success";
+        $response["messages"] = $messages;
+
+    }
+
+    echo json_encode($response);
 }
-
-echo json_encode($response);
