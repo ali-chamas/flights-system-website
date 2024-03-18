0ba1454e7816458336696fe3d2cff7982380d683
unknown <hanadi.hosri@gmail.com>
2024-03-18 21:46:48 +0200

    implementing getFlight function

--- client/scripts/pages/singleFlight.js
+++ client/scripts/pages/singleFlight.js
@@ -1,2 +1,19 @@
 const urlParams = new URLSearchParams(window.location.search);
 const flightID = urlParams.get("id");
+const departureToDestination = document.getElementById("departure-to-destination");
+const flight = {};
+
+const getFlight = async () => {
+    try {
+        const response = await fetch("http://localhost/flights-system-website/server/flights/flightsApi.php?id=2", {
+            method: "GET"
+        });
+        const responseData = await response.text();
+        flight = responseData;
+
+    } catch (error) {
+        console.error(error);
+        alert("Error occured while sending request");
+    }
+}
+ console.log(flight);
