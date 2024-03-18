641a888bb1df4ca49fd972627b491d18dd207fcf
unknown <hanadi.hosri@gmail.com>
2024-03-19 00:49:18 +0200

    fixing mistakes

--- client/scripts/pages/singleFlight.js
+++ client/scripts/pages/singleFlight.js
@@ -2,6 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const flightID = urlParams.get("id");
 const departureToDestination = document.getElementById("departure-to-destination");
 const totalRating = document.getElementById("total-rating");
+const availableTickets = document.getElementById("available-tickets");
 
 const getFlight = async () => {
     try {
@@ -17,7 +18,7 @@ const getFlight = async () => {
     }
 }
 
-const getTicket = async () => {
+const getTickets = async () => {
     try {
         const response = await fetch("http://localhost/flights-system-website/server/tickets/view.php?id=2", {
             method: "GET"
@@ -31,7 +32,6 @@ const getTicket = async () => {
     }
 }
 
-
 const app = async () =>{
     const data = await getFlight();
     const flight = JSON.parse(data);
@@ -40,6 +40,14 @@ const app = async () =>{
     const depatureTime = flight.flight.departureTime;
     const arrivalTime = flight.flight.arrivalTime;
     const rating = flight.flight.rating;
+    const ticketData = await getTickets();
+    const ticket = JSON.parse(ticketData);
+    const date = ticket.allTickets[0].date;
+    const price = ticket.allTickets[0].price;
+    const totalSeats = ticket.allTickets[0].totalSeats;
+    availableTickets.innerHTML= `Date ${date} Price ${price} Total seats ${totalSeats}`;
+
+    
     departureToDestination.innerHTML= `<h2>${departure} to ${destination}</h2> </br> Departure time ${depatureTime} </br> Arrival time ${arrivalTime}`;
     totalRating.innerHTML= rating;
 }
