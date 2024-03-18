fd505d9b51dc07544b28ffb1830b72b7a29e2f9e
unknown <hanadi.hosri@gmail.com>
2024-03-18 07:19:32 +0200

    removing unused function

--- server/tickets/modify.php
+++ server/tickets/modify.php
@@ -26,25 +26,3 @@ if (!empty($_GET["id"])) {
         "status" => "something went wrong",
     ]);
 }
-
-
-function getTicket($id) {
-    global $mysqli;
-    $query = $mysqli->prepare("select * from tickets where id = ?");
-    $query->bind_param("i", $id);
-    $query->execute();
-    $query->store_result();
-    $query->bind_result($id, $totalSeats, $date, $price, $status, $flightID);
-    $query->fetch();
-
-    $response["status"] = "success";
-    $response["ticket"] = [
-            'id' => $id,
-            'totalSeats' => $totalSeats,
-            'date' => $date,
-            'price' => $price,
-            'status' => $status,
-            'flightID' => $flightID
-    ];
-    return $response;
-}
