4ad994a8e68e1dc929246be4f7a3efb37b73c7e5
unknown <hanadi.hosri@gmail.com>
2024-03-19 05:36:52 +0200

    adding vew-airlines-reviews.php

---
+++ server/reviews/view-airlines-reviews.php
@@ -0,0 +1,37 @@
+<?php
+include("../db/connection.php");
+
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+$query = $mysqli->prepare("
+select airlinesreviews.*, users.name
+from airlinesreviews
+join users on users.id=airlinesreviews.userID
+where airlinesreviews.userID=users.id");
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+
+if($num_rows==0){
+    $response['status']='Failed';
+    $response['message']= 'no reviews available';
+} else {
+    $reviews = [];
+    $query->bind_result($id, $rating, $review, $createdAT, $userID, $airlineID, $userName);
+    while($query->fetch()) {
+        $review = [
+            'id' => $id,
+            'rating' => $rating,
+            'review' => $review,
+            'createdAT' => $createdAT,
+            'userID' => $userID,
+            'airlineID' => $airlineID,
+            'userName' => $userName
+        ];
+        $reviews[] = $review;
+    }
+    $response['status'] = 'success';
+    $response['reviews'] = $reviews;
+}  
+
+echo json_encode($response);
