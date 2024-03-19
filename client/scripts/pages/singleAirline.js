eba88dec3b26e60da838c27af5a8332ecfd576f2
unknown <hanadi.hosri@gmail.com>
2024-03-19 07:03:33 +0200

    fixing mistakes and displaying airline data

--- client/scripts/pages/singleAirline.js
+++ client/scripts/pages/singleAirline.js
@@ -1,13 +1,13 @@
 const urlParams = new URLSearchParams(window.location.search);
 const flightID = urlParams.get("id");
-const airline = document.getElementById("airline");
+const airline = document.getElementById("airline-name");
 const totalRating = document.getElementById("total-rating");
 const listOfFlights = document.getElementById("available-flights");
 const totalReviews = document.getElementById("reviews");
 
 const getAirline = async () => {
     try {
-        const response = await fetch("http://localhost/flights-system-website/server/flights/flightsApi.php?id=2", {
+        const response = await fetch("http://localhost/flights-system-website/server/airlines/airlinesApi.php?id=1", {
             method: "GET"
         });
         const responseData = await response.text();
@@ -18,3 +18,22 @@ const getAirline = async () => {
         alert("Error occured while sending request");
     }
 }
+
+
+
+
+
+
+const app = async () => {
+    const data = await getAirline();
+    const airlineData = JSON.parse(data);
+    const airlineName = airlineData.airline.name;
+    const rating = airlineData.airline.rating;
+
+    airline.innerHTML = airlineName;
+    totalRating.innerHTML = rating;
+
+
+}
+
+app();
