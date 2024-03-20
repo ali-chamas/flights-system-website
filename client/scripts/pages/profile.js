fe5299f19eb66944235f1a11ffb73f70eac824a0
unknown <hanadi.hosri@gmail.com>
2024-03-20 07:26:10 +0200

    adding request popup

--- client/scripts/pages/profile.js
+++ client/scripts/pages/profile.js
@@ -5,6 +5,9 @@ const cancelButton = document.getElementById("cancel-btn");
 const imageInput = document.getElementById("image-input");
 const usernameInput = document.getElementById("username-input");
 const emailInput = document.getElementById("email-input");
+const requestButton = document.getElementById("request-btn");
+const addRequestPopup = document.getElementById("add-request-popup");
+const amountCancelButton = document.getElementById("amount-cancel-btn");
 
 editButton.addEventListener("click", () => {
     addEditPopup.classList.remove("hidden");
@@ -12,4 +15,12 @@ editButton.addEventListener("click", () => {
 
 cancelButton.addEventListener("click", () => {
     addEditPopup.classList.add("hidden");
+});
+
+requestButton.addEventListener("click", () => {
+    addRequestPopup.classList.remove("hidden");
+});
+
+amountCancelButton.addEventListener("click", () => {
+    addRequestPopup.classList.add("hidden");
 });
