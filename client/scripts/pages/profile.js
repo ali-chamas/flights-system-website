d4ea875c1d6df8e8c2928bf1fe29b6d938972cd0
unknown <hanadi.hosri@gmail.com>
2024-03-20 06:58:16 +0200

    adding and styling popup

--- client/scripts/pages/profile.js
+++ client/scripts/pages/profile.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const editButton = document.getElementById("edit-btn");
+const addEditPopup = document.getElementById("add-edit-popup");
+const updateButton = document.getElementById("update-btn");
+const cancelButton = document.getElementById("cancel-btn");
+const imageInput = document.getElementById("image-input");
+const usernameInput = document.getElementById("username-input");
+const emailInput = document.getElementById("email-input");
+
+editButton.addEventListener("click", () => {
+    addEditPopup.classList.remove("hidden");
+});
