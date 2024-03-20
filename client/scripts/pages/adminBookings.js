c0b72b06ec600d8e89065d3a412da30e0ca291e4
Mohammadberro <ikerberro1@gmail.com>
2024-03-20 08:54:49 +0200

    Fix Popup

--- client/scripts/pages/adminBookings.js
+++ client/scripts/pages/adminBookings.js
@@ -1,9 +1,9 @@
-let editPopup = document.getElementById("edit-popup")
-let newPassengerSeat = document.getElementById("new-passenger-seat");
+const editPopup = document.getElementById("edit-popup")
+const newPassengerSeat = document.getElementById("new-passenger-seat");
 const bodyTable = document.getElementById("tbody")
+const btn = document.getElementById("btn")
 
-
-const getAllRequests = () => {
+const getAllBookings = () => {
     fetch("http://localhost/flights-system-website/server/bookings/updateBooking.php", {
         method: "GET",
     })
@@ -21,7 +21,7 @@ const getAllRequests = () => {
 };
 
 
-getAllRequests();
+getAllBookings();
 
 function addPopup(){
     editPopup.classList.add("open-popup");
@@ -33,7 +33,7 @@ function removePopup(){
 }
 
 
-function renderLoadedData(data){
+const renderLoadedData = (data) =>{
     bodyTable.innerHTML = "";
 
         data.bookings.forEach((booking) => {
@@ -41,7 +41,7 @@ function renderLoadedData(data){
         });
 }
 
-function addTable(booking){
+const addTable = (booking) => {
     bodyTable.innerHTML += ` <tr id="${booking.id}">
     <td class="text-align line-right">${booking.passenger_name}</td>
     <td class="text-align line-right">${booking.id}</td>
@@ -50,16 +50,17 @@ function addTable(booking){
     <td class="text-align line-right">${booking.departure}</td>
     <td class="text-align line-right">${booking.destination}</td>
     <td class="text-align"><button class="accept fa solid fa-pen-to-square text-secondary action-button" onclick=addPopup();></button>
-    <button class="reject fa-solid fa-x text-secondary action-button";></button>
+    <button class="reject fa-solid fa-x text-secondary action-button" onclick=deleteBooking();></button>
     </td>
     </tr> `;
 }
 
-function formIsEmpty(form) { 
-    let inputs = form.getElementsByTagName('input')
-    console.log(inputs)
-  for (i = 0; i < inputs.length; i++) { 
-    if (inputs[i].value.length == 0) return true; 
-  } 
-  return false; 
+const modifySeat = () => {
+    newSeatNumber = newPassengerSeat.val;
+    if (!newSeatNumber){
+        alert("Chose a New Seat Number");
+    }
+    else{
+        updateSeatNumber(newSeatNumber);
+    }
 }
