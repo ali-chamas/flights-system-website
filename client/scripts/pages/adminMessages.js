68b9475545a030adef9f284c0dff3d7a6cd9783b
unknown <hanadi.hosri@gmail.com>
2024-03-21 01:32:38 +0200

    adding message cards from js

--- client/scripts/pages/adminMessages.js
+++ client/scripts/pages/adminMessages.js
@@ -1,1 +1,55 @@
-console.log("hello world");
+let messageCardContainer = document.getElementById("message-card-container");
+
+let messages = [];
+
+
+const getMessages = async () => {
+    try {
+        const response = await fetch("http://localhost/flights-system-website/server/messages/receive.php", {method: "GET"});
+        const data = await response.json();
+        messages = data.messages;
+
+    } catch (error) {
+        console.error(error);
+        alert("Error occured while sending request");
+    }
+}
+
+const generateMessages = () => {
+    messageCardContainer.innerHTML = "";
+    messages.forEach((message) => {
+        messageCardContainer.innerHTML += `<div class="message-card">
+        <div class="image">
+        <img src="${message.senderImage}" />
+        </div>
+        <div class="user-message">
+            <div class="user">
+                <p>${message.senderUsername}</p>
+            </div>
+            <div class="message">
+                <p>${message.message}</p>
+            </div>
+        </div>
+        <div>
+        <button class="button">Chat back</button>
+        </div>
+        </div>`
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+const app = async() => {
+    await getMessages();
+    generateMessages();
+}
+
+app();
