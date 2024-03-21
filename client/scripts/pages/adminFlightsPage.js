b13aa26ce48b2e28fbcb03b18359300d627113f0
Mohammadberro <ikerberro1@gmail.com>
2024-03-21 04:06:40 +0200

    Create adminFlightsPage

---
+++ client/scripts/pages/adminFlightsPage.js
@@ -0,0 +1,65 @@
+const flightsContainer = document.getElementById("flights-container");
+const filterBtn = document.getElementById("flights-filter");
+
+let flights = [];
+let filterValue = "any";
+
+const getFlights = async () => {
+  try {
+    const res = await fetch(`${apiURL}/flights/flightsApi.php`);
+    const data = await res.json();
+    flights = data.flights;
+    console.log(flights);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const generateFlights = (array) => {
+  flightsContainer.innerHTML = "";
+  array.forEach((a) => {
+    flightsContainer.innerHTML += `<a
+                                        href="/client/pages/flights/singleFlightPage.html?id=${a.id}"
+                                        class="flex column gap flight-card"
+                                        >
+                                        <img src="${a.image}" alt="" />
+                                        <div class="flex column gap p">
+                                        <div class="w-full flex justify-between">
+                                        <div class="flex column gap">
+                                        <b>From</b>
+                                        <p>${a.departure}</p>
+                                        </div>
+                                        <div class="flex column gap">
+                                        <b>To</b>
+                                        <p>${a.destination}</p></div>
+                                        </div>
+                                        
+                                        <div class="w-full flex justify-between">
+                                            <small>${a.airlineName} flights</small>
+                                            <small><i class="fa-solid fa-star rate-color"></i> ${a.rating}</small>
+                                        </div>
+                                        </div>
+                                        </a>`;
+  });
+};
+
+const filterFlights = (key) => {
+  if (key == "rating") {
+    const newFlights = flights.sort((a, b) => b.rating - a.rating);
+    generateFlights(newFlights);
+  } else if (key == "all") {
+    const newFlights = flights.sort((a, b) => b.id - a.id);
+    generateFlights(newFlights);
+  }
+};
+
+const app = async () => {
+  await getFlights();
+  generateFlights(flights);
+  filterBtn.addEventListener("change", (e) => {
+    filterFlights(e.target.value);
+  });
+};
+
+app();
+
